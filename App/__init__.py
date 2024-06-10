14fbf6cc9c392d480ec1612ce7eb9e937a3d9c7f
Mohamed-Aidarus <maxamedmorgan3@gmail.com>
2024-06-10 10:10:35 +0300

    App/__init__.py

--- App/__init__.py
+++ App/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_cors import CORS
+from flask_cors import COR
 
 def create_app():
     app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
