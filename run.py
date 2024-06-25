f25ef4b460e46dc914127ae213d2a267b0885f2f
Abdilaahi_Dhaqane <Abdilaahimowliid@gmail.com>
2024-06-25 11:22:14 +0300

    new

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 import sys
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Retrieve host and port from environment variables
+host = os.getenv('HOST', '127.0.0.1')
+port = os.getenv('PORT', 5000)
 
 # Add the parent directory to the Python path
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
@@ -10,4 +18,4 @@ if __name__ == '__main__':
     import logging
     logging.basicConfig(level=logging.INFO)
     app = create_app()
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host=host, port=int(port), debug=True)
